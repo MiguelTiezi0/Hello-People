7fcb4bd69ef6c811bfb09782c0903b3a5b0b4161
VitorAugusto88 <visilvavidal@gmail.com>
2025-01-06 10:26:09 -0300

    First Commit

---
+++ Infrastructure/Data/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreManager.Models;
+using System.Collections.Generic;
+
+namespace StoreManager.Infrastructure.Data.Context
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CategoryClient> CategoryClient { get; set; }
+
+    
+
+    }
+}
---
+++ Models/CategoryClient.cs
@@ -0,0 +1,18 @@
+﻿namespace StoreManager.Models
+{
+    public class CategoryClient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string RG { get; set; }
+        public string CPF { get; set; }
+        public string DataDeNascimento { get; set; }
+        public string Histórico { get; set; }
+        public string Endereço { get; set; }
+        public string Telefone { get; set; }
+        public string Credaiário { get; set; }
+        public string Consignação { get; set; }
+
+
+    }
+}
